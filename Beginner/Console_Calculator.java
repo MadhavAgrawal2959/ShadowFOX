804d1bc9e92aa34d9180a6c860f996fec3a72013
Madhav Agrawal <madhavagrawal2959@gmail.com>
2024-10-10 19:02:32 +0530

    Implemented Percentage and Log functions

--- Beginner/Console_Calculator.java
+++ Beginner/Console_Calculator.java
@@ -9,7 +9,7 @@ public class Console_Calculator {
         System.out.println("Welcome to Console Calculator in Java");
         System.out.println("Select the type of operation you want to perform:");
         System.out.println("1. Airthmatic Operations");
-        System.out.println("2. Scientific Operations");
+        System.out.println("2. Logarithmic Exponential Operations");
         System.out.println("3. Unit Conversion");
         System.out.println("4. Perccentage Calculation");
         System.out.println("5.Exit");
@@ -27,16 +27,18 @@ public class Console_Calculator {
                     another_operation = scanner.nextBoolean();
                     if (another_operation) {
                         main(null);
+                    } else {
+                        break;
                     }
-                    break;
                 case 2:
                     Logarithmic_Exponential_Operations();
                     System.out.println("Do you want to perform another operation? (true/false)");
                     another_operation = scanner.nextBoolean();
                     if (another_operation) {
                         main(null);
+                    } else {
+                        break;
                     }
-                    break;
                 case 3:
                     Unit_Conversion();
                     System.out.println("Do you want to perform another operation? (true/false)");
@@ -71,6 +73,7 @@ public class Console_Calculator {
         System.out.println("7. Exit");
         System.out.println("Enter your choice: ");
         int choice = scanner.nextInt();
+        boolean change;
 
         if (choice == 7) {
             return;
@@ -99,15 +102,25 @@ public class Console_Calculator {
                         System.out.println("Error: Division by zero is not allowed.");
                     }
                     System.out.println("want to rechange the number2 ?(true/false)");
-                    boolean change = scanner.nextBoolean();
+                    change = scanner.nextBoolean();
                     if (change) {
                         num2 = scanner.nextInt();
                         System.out.println("the sum of the Two numbers is: " + (num1 / num2));
                     }
                     break;
                 case 5:
-                    System.out.println("the sum of the Two numbers is: " + (num1 % num2));
-                    break;
+                    try {
+                        int result = (int) num1 / (int) num2;
+                        System.out.println("the Division of two no is" + result);
+                    } catch (ArithmeticException e) {
+                        System.out.println("Error: Division by zero is not allowed.");
+                    }
+                    System.out.println("want to rechange the number2 ?(true/false)");
+                    change = scanner.nextBoolean();
+                    if (change) {
+                        num2 = scanner.nextInt();
+                        System.out.println("the sum of the Two numbers is: " + (num1 % num2));
+                    }
                 default:
                     throw new AssertionError();
             }
@@ -262,10 +275,132 @@ public class Console_Calculator {
     }
 
     private static void Logarithmic_Exponential_Operations() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        System.out.println("Select the type of operation you want to perform");
+        System.out.println("1. Logarithmic Operations");
+        System.out.println("2. Exponential Operations");
+        System.out.println("3. Back to Main Menu");
+        System.out.println("4. Exit");
+        System.out.println("Enter your choice: ");
+        int choice = scanner.nextInt();
+        if (choice == 4) {
+            return;
+        } else if (choice == 3) {
+            main(null);
+        } else {
+            switch (choice) {
+                case 1:
+                    Logarithmic_Operations();
+                    break;
+                case 2:
+                    Exponential_Operations();
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        }
+    }
+
+    private static void Exponential_Operations() {
+        System.out.println("Select the type of operation you want to perform");
+        System.out.println("1. Square");
+        System.out.println("2. Cube");
+        System.out.println("3. Power");
+        System.out.println("4. Back to Main Menu");
+        System.out.println("5. Exit");
+        System.out.println("Enter your choice: ");
+        int choice = scanner.nextInt();
+        if (choice == 5) {
+            return;
+        } else if (choice == 4) {
+            main(null);
+        } else {
+            System.out.println("Enter the number: ");
+            float num = scanner.nextFloat();
+            switch (choice) {
+                case 1:
+                    System.out.println("Square: of the number is " + (num * num));
+                    break;
+                case 2:
+                    System.out.println("Cube: of  number is" + (num * num * num));
+                    break;
+                case 3:
+                    System.out.println("Enter the power: ");
+                    float power = scanner.nextFloat();
+                    System.out.println("Power: raised of numer " + Math.pow(num, power));
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        }
+    }
+
+    private static void Logarithmic_Operations() {
+        System.out.println("Select the type of operation you want to perform");
+        System.out.println("1. Logarithm to the base 10");
+        System.out.println("2. Logarithm to the base 2");
+        System.out.println("3. Natural Logarithm");
+        System.out.println("4. Back to Main Menu");
+        System.out.println("5. Exit");
+        System.out.println("Enter your choice: ");
+        int choice = scanner.nextInt();
+        if (choice == 5) {
+            return;
+        } else if (choice == 4) {
+            main(null);
+        } else {
+            System.out.println("Enter the number: ");
+            float num = scanner.nextFloat();
+            switch (choice) {
+                case 1:
+                    System.out.println("Logarithm to the base 10: " + Math.log10(num));
+                    break;
+                case 2:
+                    System.out.println("Logarithm to the base 2: " + Math.log(num) / Math.log(2));
+                    break;
+                case 3:
+                    System.out.println("Natural Logarithm: " + Math.log(num));
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        }
     }
 
     private static void Percentage_Calculation() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        System.out.println("Select the type of operation you want to perform");
+        System.out.println("1. Percentage of a number");
+        System.out.println("2. Percentage Increase");
+        System.out.println("3. Percentage Decrease");
+        System.out.println("4. Back to Main Menu");
+        System.out.println("5. Exit");
+        System.out.println("Enter your choice: ");
+        int choice = scanner.nextInt();
+        if (choice == 5) {
+            return;
+        } else if (choice == 4) {
+            main(null);
+        } else {
+            System.out.println("Enter the number: ");
+            float num = scanner.nextFloat();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the percentage: ");
+                    float percentage = scanner.nextFloat();
+                    System.out.println("Percentage of the number: " + (num * percentage / 100));
+                    break;
+                case 2:
+                    System.out.println("Enter the percentage increase: ");
+                    float increase = scanner.nextFloat();
+                    System.out.println("Percentage Increase: " + (num + num * increase / 100));
+                    break;
+                case 3:
+                    System.out.println("Enter the percentage decrease: ");
+                    float decrease = scanner.nextFloat();
+                    System.out.println("Percentage Decrease: " + (num - num * decrease / 100));
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        }
     }
 }
