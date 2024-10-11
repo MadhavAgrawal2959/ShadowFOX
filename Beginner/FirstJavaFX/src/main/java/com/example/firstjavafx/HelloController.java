8499b51351aa7e7ebc6bcb6244c4afc70eba30ad
Madhav Agrawal <madhavagrawal2959@gmail.com>
2024-10-11 19:20:30 +0530

    Getting started with JavaFx

--- Beginner/FirstJavaFX/src/main/java/com/example/firstjavafx/HelloController.java
+++ Beginner/FirstJavaFX/src/main/java/com/example/firstjavafx/HelloController.java
@@ -1,13 +1,22 @@
 package com.example.firstjavafx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 public class HelloController {
 
     @FXML
-    void printHelloWorld(MouseEvent event) {
-        System.out.println("Hello World!");
+    private TextField namein;
+
+    @FXML
+    private Text nameout;
+
+    @FXML
+    void namechange(MouseEvent event) {
+        nameout.setText(namein.getText());
+
     }
 
 }
